cae5c63db55a6099d16f53394c4a98beb330b89b
Pavl0ffN <pavl0ffn94@gmail.com>
2023-05-07 20:03:03 +0400

    add service

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './style/style.css';
 import Header from './modules/header/Header';
 import Nav from './modules/nav/Nav';
 import Footer from './modules/footer/Footer';
+import Service from './modules/servic/Service';
 
 function App() {
   return (
     <div className="App">
       <Nav />
       <Header />
+      <Service />
       <Footer />
     </div>
   );
